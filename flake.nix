834eb319f9ed77b54e787c1f591e92ff0f2abd37
Florian Zier <9168602+zierf@users.noreply.github.com>
2024-09-07 15:53:35 +0200

    test PySide v6.6.0
    use legacy nixpkgs and poetry2nix to verify overrides for PySide v6.6.0
    -> successful build, errors at runtime
    
    ```SH
    $> nix run
    
    Traceback (most recent call last):
      File "/nix/store/53vxf97jval3xcfsx1lgfhcqcgk5df6x-python3.10-poetry2nix-example-0.1.0/bin/..poetry2nix-example-wrapped-wrapped", line 6, in <module>
        from poetry2nix_example.example import main
      File "/nix/store/53vxf97jval3xcfsx1lgfhcqcgk5df6x-python3.10-poetry2nix-example-0.1.0/lib/python3.10/site-packages/poetry2nix_example/example.py", line 1, in <module>
        from PySide6.QtCore import QPoint
    ModuleNotFoundError: No module named 'PySide6.QtCore'
    ```
    
    ```SH
    $> python -m poetry2nix_example.example
    
    Traceback (most recent call last):
      File "/nix/store/s6fgyqbk8vn1014daznm5kqx90xdn86x-python3-3.10.13/lib/python3.10/runpy.py", line 196, in _run_module_as_main
        return _run_code(code, main_globals, None,
      File "/nix/store/s6fgyqbk8vn1014daznm5kqx90xdn86x-python3-3.10.13/lib/python3.10/runpy.py", line 86, in _run_code
        exec(code, run_globals)
      File "/home/zierf/workspaces/Python/PySide6Minimal/poetry2nix_example/example.py", line 1, in <module>
        from PySide6.QtCore import QPoint
    ModuleNotFoundError: No module named 'PySide6.QtCore'
    ```

--- flake.nix
+++ flake.nix
@@ -1,13 +1,15 @@
 {
-  description = "Minimal Example for Nix2Poetry";
+  description = "Example for legacy Nix2Poetry with PySide 6.6.0";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # https://github.com/NixOS/nixpkgs/tree/HEAD@%7B2023-10-26%7D
+    nixpkgs.url = "github:nixos/nixpkgs/d8bb0bda47dbe7fb569c6b4c6d46d349baae6e5d";
 
     flake-utils.url = "github:numtide/flake-utils";
 
+    # https://github.com/nix-community/poetry2nix/pull/1356
     poetry2nix = {
-      url = "github:nix-community/poetry2nix";
+      url = "github:nix-community/poetry2nix/2d29b1692e6f99eab700a4f786028f0b34cde1ac";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -23,185 +25,111 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system}.extend poetry2nix.overlays.default;
+        pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (poetry2nix.lib.mkPoetry2Nix { inherit pkgs; })
-          mkPoetryApplication
-          mkPoetryEnv
-          defaultPoetryOverrides
-          ;
+        myApp = pkgs.poetry2nix.mkPoetryApplication rec {
+          src = self;
 
-        myApp = mkPoetryApplication {
-          projectDir = self;
+          pyproject = ./pyproject.toml;
+          poetrylock = ./poetry.lock;
 
-          exeName = "poetry2nix-example";
+          python = pkgs.python310;
 
-          python = pkgs.python312;
+          overrides = pkgs.poetry2nix.overrides.withDefaults (
+            self: super: {
+              pyside6-essentials = super.pyside6-essentials.overridePythonAttrs (old: {
+                autoPatchelfIgnoreMissingDeps = [ "libmysqlclient.so.21" "libmimerapi.so" "libQt6*" ];
+                preFixup = ''
+                  addAutoPatchelfSearchPath $out/${self.python.sitePackages}/PySide6
+                  addAutoPatchelfSearchPath ${self.shiboken6}/${self.python.sitePackages}/shiboken6
+                '';
+                postInstall = ''
+                  rm -r $out/${self.python.sitePackages}/PySide6/__pycache__
+                '';
+                propagatedBuildInputs = old.propagatedBuildInputs ++ [
+                  pkgs.libxkbcommon
+                  pkgs.gtk3
+                  pkgs.speechd
+                  pkgs.gst
+                  pkgs.gst_all_1.gst-plugins-base
+                  pkgs.gst_all_1.gstreamer
+                  pkgs.postgresql.lib
+                  pkgs.unixODBC
+                  pkgs.pcsclite
+                  pkgs.xorg.libxcb
+                  pkgs.xorg.xcbutil
+                  pkgs.xorg.xcbutilcursor
+                  pkgs.xorg.xcbutilerrors
+                  pkgs.xorg.xcbutilimage
+                  pkgs.xorg.xcbutilkeysyms
+                  pkgs.xorg.xcbutilrenderutil
+                  pkgs.xorg.xcbutilwm
+                  pkgs.libdrm
+                  pkgs.pulseaudio
+                  self.shiboken6
+                ];
+              });
 
-          preferWheels = true;
-
-          # extend official overrides
-          # https://github.com/nix-community/poetry2nix/blob/7619e43c2b48c29e24b88a415256f09df96ec276/overrides/default.nix#L2743-L2805
-          overrides = defaultPoetryOverrides.extend (
-            final: prev: {
-              # working Overrides for PySide6 via nixpkgs
-              # https://github.com/nix-community/poetry2nix/issues/1191#issuecomment-1707590287
-              #pyside6 = final.pkgs.python312.pkgs.pyside6;
-              #shiboken6 = final.pkgs.python3.pkgs.shiboken6;
-
-              pyside6 = prev.pyside6.overridePythonAttrs (
-                old:
-                pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-                  dontWrapQtApps = true;
-
-                  # also try PatchElf search paths for main package
-                  preFixup = ''
-                    addAutoPatchelfSearchPath $out/${final.python.sitePackages}/PySide6
-                    addAutoPatchelfSearchPath ${final.shiboken6}/${final.python.sitePackages}/shiboken6
-                  '';
-                  postInstall = ''
-                    rm -r $out/${final.python.sitePackages}/PySide6/__pycache__
-                  '';
-
-                  propagatedBuildInputs = old.propagatedBuildInputs or [ ] ++ [
-                    pkgs.qt6.qtmultimedia
-
-                    # dependencies from nixpkgs pyside6
-                    # https://github.com/NixOS/nixpkgs/blob/c40ce90d28e607d3b967963a0240c43d1210dbc5/pkgs/development/python-modules/pyside6/default.nix
-                    pkgs.qt6.qtbase
-                    # optional
-                    pkgs.qt6.qt3d
-                    pkgs.qt6.qtcharts
-                    pkgs.qt6.qtconnectivity
-                    pkgs.qt6.qtdatavis3d
-                    pkgs.qt6.qtdeclarative
-                    pkgs.qt6.qthttpserver
-                    pkgs.qt6.qtmultimedia
-                    pkgs.qt6.qtnetworkauth
-                    pkgs.qt6.qtquick3d
-                    pkgs.qt6.qtremoteobjects
-                    pkgs.qt6.qtscxml
-                    pkgs.qt6.qtsensors
-                    pkgs.qt6.qtspeech
-                    pkgs.qt6.qtsvg
-                    pkgs.qt6.qtwebchannel
-                    pkgs.qt6.qtwebsockets
-                    pkgs.qt6.qtpositioning
-                    pkgs.qt6.qtlocation
-                    pkgs.qt6.qtshadertools
-                    pkgs.qt6.qtserialport
-                    pkgs.qt6.qtserialbus
-                    pkgs.qt6.qtgraphs
-                    pkgs.qt6.qttools
-                  ];
-                }
-              );
-
-              pyside6-essentials = prev.pyside6-essentials.overridePythonAttrs (
-                old:
-                pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-                  # prevent error: 'Error: wrapQtAppsHook is not used, and dontWrapQtApps is not set.'
-                  dontWrapQtApps = true;
-
-                  # added libgbm.so.1 to satsify missing library for auto-patchelf
-                  autoPatchelfIgnoreMissingDeps = [
-                    "libmysqlclient.so.21"
-                    "libmimerapi.so"
-                    "libQt6*"
-                    "libgbm.so.1"
-                  ];
-                  preFixup = ''
-                    addAutoPatchelfSearchPath $out/${final.python.sitePackages}/PySide6
-                    addAutoPatchelfSearchPath ${final.shiboken6}/${final.python.sitePackages}/shiboken6
-                  '';
-                  postInstall = ''
-                    rm -r $out/${final.python.sitePackages}/PySide6/__pycache__
-                  '';
-                  propagatedBuildInputs = old.propagatedBuildInputs or [ ] ++ [
-                    # PySide6 in nixpkgs needs these for video playback
-                    pkgs.qt6.qtmultimedia
-                    pkgs.xorg.libXrandr
-                    # enable hardware acceleration
-                    pkgs.libva
-                    pkgs.openssl
-                    # satisfy some missing libraries for auto-patchelf patching PySide6-Essentials
-                    pkgs.qt6.qtquick3d
-                    pkgs.qt6.qtvirtualkeyboard
-                    pkgs.qt6.qtwebengine
-                    # other packages from default override
-                    pkgs.libxkbcommon
-                    pkgs.gtk3
-                    pkgs.speechd
-                    pkgs.gst
-                    pkgs.gst_all_1.gst-plugins-base
-                    pkgs.gst_all_1.gstreamer
-                    pkgs.postgresql.lib
-                    pkgs.unixODBC
-                    pkgs.pcsclite
-                    pkgs.xorg.libxcb
-                    pkgs.xorg.xcbutil
-                    pkgs.xorg.xcbutilcursor
-                    pkgs.xorg.xcbutilerrors
-                    pkgs.xorg.xcbutilimage
-                    pkgs.xorg.xcbutilkeysyms
-                    pkgs.xorg.xcbutilrenderutil
-                    pkgs.xorg.xcbutilwm
-                    pkgs.libdrm
-                    pkgs.pulseaudio
-                    final.shiboken6
-                  ];
-                }
-              );
-
-              pyside6-addons = prev.pyside6-addons.overridePythonAttrs (
-                old:
-                pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
-                  dontWrapQtApps = true;
-
-                  autoPatchelfIgnoreMissingDeps = [
-                    "libmysqlclient.so.21"
-                    "libmimerapi.so"
-                    "libQt6Quick3DSpatialAudio.so.6"
-                    "libQt6Quick3DHelpersImpl.so.6"
-                  ];
-                  preFixup = ''
-                    addAutoPatchelfSearchPath ${final.shiboken6}/${final.python.sitePackages}/shiboken6
-                    addAutoPatchelfSearchPath ${final.pyside6-essentials}/${final.python.sitePackages}/PySide6
-                  '';
-                  propagatedBuildInputs = old.propagatedBuildInputs or [ ] ++ [
-                    pkgs.nss
-                    pkgs.xorg.libXtst
-                    pkgs.alsa-lib
-                    pkgs.xorg.libxshmfence
-                    pkgs.xorg.libxkbfile
-                  ];
-                  postInstall = ''
-                    rm -r $out/${final.python.sitePackages}/PySide6/__pycache__
-                  '';
-                }
-              );
+              pyside6-addons = super.pyside6-addons.overridePythonAttrs (old: {
+                autoPatchelfIgnoreMissingDeps = [
+                  "libmysqlclient.so.21"
+                  "libmimerapi.so"
+                  "libQt6Quick3DSpatialAudio.so.6"
+                  "libQt6Quick3DHelpersImpl.so.6"
+                ];
+                preFixup = ''
+                  addAutoPatchelfSearchPath ${self.shiboken6}/${self.python.sitePackages}/shiboken6
+                  addAutoPatchelfSearchPath ${self.pyside6-essentials}/${self.python.sitePackages}/PySide6
+                '';
+                propagatedBuildInputs = old.propagatedBuildInputs ++ [
+                  pkgs.nss
+                  pkgs.xorg.libXtst
+                  pkgs.alsa-lib
+                  pkgs.xorg.libxshmfence
+                  pkgs.xorg.libxkbfile
+                ];
+                postInstall = ''
+                  rm -r $out/${self.python.sitePackages}/PySide6/__pycache__
+                '';
+              });
             }
           );
 
           pythonRelaxDeps = [ ];
 
-          buildInputs = (
+          dependencies = (
             with pkgs;
             [
-              qt6.qtbase
-              qt6.qtmultimedia
+              # stdenv.cc.cc.lib
+              # dbus
+              # fontconfig
+              # freetype
+              # glib
+              # libGL
+              # libkrb5
+              # libpulseaudio
+              # libva
+              # libxkbcommon
+              # openssl
+              # qt6.full
+              # wayland
+              # xorg.libX11
+              # xorg.libxcb
+              # xorg.libXi
+              # xorg.libXrandr
             ]
           );
 
-          nativeBuildInputs = (
-            with pkgs;
-            [
+          buildInputs = (with pkgs; [ qt6.qtbase ]) ++ dependencies;
+
+          nativeBuildInputs =
+            (with pkgs; [
               makeWrapper
               qt6.wrapQtAppsHook
-            ]
-          );
+            ])
+            ++ dependencies;
 
-          propogatedBuildInputs = (with pkgs; [ ]);
+          propogatedBuildInputs = (with pkgs; [ ]) ++ dependencies;
         };
       in
       {
@@ -224,33 +152,11 @@
         # $> python -m poetry2nix_example.example => launch application as Python module
         # $> poetry run poetry2nix-example        => execute the application via Poetry
         devShells.default = pkgs.mkShell rec {
-          #inputsFrom = [ self.apps.${system}.default ];
           inputsFrom = [ self.packages.${system}.myApp ];
 
-          packages = with pkgs; [
-            poetry
-          ];
+          packages = with pkgs; [ poetry ] ++ myApp.dependencies;
 
-          buildInputs = (
-            with pkgs;
-            [
-              stdenv.cc.cc.lib
-              dbus
-              fontconfig
-              freetype
-              glib
-              libGL
-              libkrb5
-              libpulseaudio
-              libva
-              libxkbcommon
-              openssl
-              # video playback
-              xorg.libXrandr
-            ]
-          );
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (packages ++ buildInputs);
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath packages;
         };
 
         # Shell for poetry.
@@ -260,9 +166,7 @@
         # $> poetry install
         #
         # Use this shell for changes to pyproject.toml and poetry.lock.
-        devShells.poetry = pkgs.mkShell {
-          packages = [ pkgs.poetry ];
-        };
+        devShells.poetry = pkgs.mkShell { packages = [ pkgs.poetry ]; };
       }
     );
 }
